7efbe00efc42b464ffcbf5340ea48bc2a7fb2bcb
Gerrit Botha <gerrit.botha@uct.ac.za>
2023-09-08 20:12:58 +0200

    Adding additional packeges requried for tidyverse and set final instance count

--- instance.tf
+++ instance.tf
@@ -15,18 +15,18 @@ resource "openstack_compute_keypair_v2" "terraform-key" {
 
 resource "openstack_networking_floatingip_v2" "base" {
   pool = "${var.floating_ip_pool}"
-  count = 2
+  count = 25
 }
 
 resource "openstack_compute_floatingip_associate_v2" "base" {
-  count = 2
+  count = 25
   floating_ip = openstack_networking_floatingip_v2.base[count.index].address
   instance_id = openstack_compute_instance_v2.base[count.index].id
 }
 
 resource "openstack_compute_instance_v2" "base" {
-    count = 2
-    name            = "${var.server_name}-${count.index + 26}"
+    count = 25
+    name            = "${var.server_name}-${count.index + 1}"
     image_name      = "20230405-jammy"
     flavor_name     = "ilifu-A"
     image_id        = "e4dda219-c90f-4c8b-8c02-beff460b0be0"
